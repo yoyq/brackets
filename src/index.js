1a73a2c5db4d5180d89658296ad1d80632c68071
dima <3277898@gmail.com>
2019-02-25 22:44:05 +0300

    brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsMas = [];
+    let openBrackets = {};
+    let closeBrackets = {};
+    let sameBrackets = {};
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
+            sameBrackets[bracketsConfig[i][0]] = bracketsConfig[i][0];
+        } else {
+            openBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+            closeBrackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+        }
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (sameBrackets[str[i]]) {
+            if (bracketsMas[bracketsMas.length-1] === str[i]) {
+                bracketsMas.pop();
+            } else {
+                bracketsMas.push(str[i]);
+            }
+        } else if (openBrackets[str[i]]) {
+            bracketsMas.push(str[i]);
+        } else {
+            if (closeBrackets[str[i]] && bracketsMas[bracketsMas.length-1] === closeBrackets[str[i]]) {
+                bracketsMas.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return bracketsMas.length === 0;
 }
